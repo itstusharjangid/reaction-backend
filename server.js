c3e0de66baaf059ce6392bdc71c4a1f0a6930078
itstusharjangid <tashujangid52@gmail.com>
2025-09-29 03:48:34 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+// server.js
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    console.log(`User ${socket.id} joined room ${roomId}`);
+  });
+
+  socket.on("signal", (data) => {
+    socket.to(data.room).emit("signal", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected:", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
